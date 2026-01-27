f2d54e0c28bf94122d7efa68e794757db9ca0c7f
Alan Xinhe Liu <liuxinhe315@gmail.com>
2026-01-27 16:07:42 +0800

    fix frontend public src fetch bug

--- App.tsx
+++ App.tsx
@@ -21,7 +21,7 @@ const App: React.FC = () => {
   useEffect(() => {
     const fetchMenu = async () => {
       try {
-        const response = await fetch('./defaultMenu.json');
+        const response = await fetch('/defaultMenu.json');
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
